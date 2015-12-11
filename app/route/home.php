a28649d9d2072dad4dfaf5f68bbb1b3765ab6168
remi.r <tekreme73@gmail.com>
2015-12-11 10:13:19 +0000

    First try with EasyRdf

--- app/route/home.php
+++ app/route/home.php
@@ -1,9 +1,12 @@
 <?php
 
 $app->get('/', function() use ($app){
-    
+
+    $foaf = new EasyRdf_Graph("http://njh.me/foaf.rdf");
+    $foaf->load();
+    $me = $foaf->primaryTopic();
     
     $app->render('home', [
-        'nom'   => 'Bob',
+        'nom'   => $me->get('foaf:name'),
     ]);
 })->name('home');
